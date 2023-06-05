bfb38fad19308464eb0e941b01ad1d9ffce1e1b8
Domeenik <dominik@reifer.de>
2023-06-05 19:10:33 +0200

    added some config

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ c_zmq_addr = config.get("zmq", "address")
 c_zmq_port = config.get("zmq", "port")
 c_zmq_epoc_time = config.get("zmq", "epoch_time")
 
+c_freq = config.get("game", "updates_per_second")
+
 # setup ZeroMQ-connection
 context = zmq.Context()
 print(f"[INFO] connect to server 'tcp://{c_zmq_addr}:{c_zmq_port}'")
@@ -29,7 +31,7 @@ time_start = time.time()
 time_a = time_start
 while True:
     time_b = time.time()
-    if (time_b >= time_a + 1):
+    if (time_b >= time_a + (1./c_freq)):
         time_a = time_b
         
         # get timestamp
